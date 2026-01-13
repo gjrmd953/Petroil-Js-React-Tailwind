427ecaaa65c69fead19dfa8d4ae2032f5d7cf5e0
gjrmd953 <gjrmd953@gmail.com>
2026-01-13 23:34:08 +0600

    edit

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import MapImg from "../assets/map.png"
+import Container from './Container'
 
 const Map = () => {
-  return (
-    <div>
-        <div className="">
-            <img src={MapImg} alt="" />
+    return (
+        <div>
+            <div className="">
+                <img src={MapImg} alt="" />
+            </div>
+            <div className="bg-[#F40404]">
+                <Container>
+                    <div className="flex justify-between py-12">
+                        <h2 className=' font-pop font-bold text-4xl text-praimary'>
+                            Want to join as member branch in your area?
+                        </h2>
+                        <button className=' py-3 px-8 w-fit border-2 font-pop font-semibold text-praimary cursor-pointer'>
+                            CONTACT
+                        </button>
+                    </div>
+                </Container>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Map
