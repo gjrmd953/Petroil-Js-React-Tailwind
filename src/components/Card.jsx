ec0df12b293d22eeca7709a9df8575a85ba2b661
gjrmd953 <gjrmd953@gmail.com>
2026-01-13 23:15:26 +0600

    edit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,38 +2,38 @@ import React from 'react'
 import Container from './Container'
 
 const Card = () => {
-  return (
-    <div className='py-28'>
-             <Container>
-                <div className="flex justify-between gap-16">
+    return (
+        <div className='py-28'>
+            <Container>
+                <div className="flex justify-between gap-18">
                     <div className="w-1/3 bg-[#00000099]">
-                         <h2>
-                            lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.
-                         </h2>
-                         <button>
+                        <h2 className=' font-pop font-bold text-praimary text-2xl pl-13 pt-18 '>
+                            lorem ipsum dolor <br />sit amet <br /> consectetur <br /> adipiscing elit sed <br /> do eiusmod.
+                        </h2>
+                        <button className=' font-pop font-semibold text-praimary mt-10 ml-13 mb-14 py-2.5 px-4 bg-[#FFFFFF80] cursor-pointer'>
                             Read more
-                         </button>
+                        </button>
                     </div>
                     <div className="w-1/3 bg-[#00000099]">
-                         <h2>
-                            lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.
-                         </h2>
-                         <button>
+                        <h2 className='font-pop font-bold text-praimary text-2xl pl-13 pt-18 '>
+                            lorem ipsum dolor <br />sit amet <br /> consectetur <br /> adipiscing elit sed <br /> do eiusmod.
+                        </h2>
+                        <button className='font-pop font-semibold text-praimary mt-10 ml-13 mb-14 py-2.5 px-4 bg-[#FFFFFF80] cursor-pointer'>
                             Read more
-                         </button>
+                        </button>
                     </div>
                     <div className="w-1/3 bg-[#00000099]">
-                         <h2>
-                            lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.
-                         </h2>
-                         <button>
+                        <h2 className='font-pop font-bold text-praimary text-2xl pl-13 pt-18 '>
+                            lorem ipsum dolor <br />sit amet <br /> consectetur <br /> adipiscing elit sed <br /> do eiusmod.
+                        </h2>
+                        <button className='font-pop font-semibold text-praimary mt-10 ml-13 mb-14 py-2.5 px-4 bg-[#FFFFFF80] cursor-pointer'>
                             Read more
-                         </button>
+                        </button>
                     </div>
                 </div>
-             </Container>
-    </div>
-  )
+            </Container>
+        </div>
+    )
 }
 
 export default Card
