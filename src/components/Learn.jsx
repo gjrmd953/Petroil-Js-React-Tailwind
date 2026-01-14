908e421c46b7efeb4847a2b6d30268167fbc82d8
gjrmd953 <gjrmd953@gmail.com>
2026-01-15 01:20:48 +0600

    edit

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -8,12 +8,16 @@ const Learn = () => {
             <Container>
                 <div className="flex">
                     <div className="w-1/3">
-                        <h2>
-                            Learn more about our company
+                        <div className="bg-[#F40404] h-full">
+                            <h2 className='font-pop py-24 pl-[90px] font-bold text-4xl text-praimary leading-tight'>
+                            Learn more <br /> about our <br /> company
                         </h2>
+                        </div>
                     </div>
                     <div className="w-2/3">
-                        <img src={LearnImg} alt="" />
+                        <div className="h-full bg-cover bg-center relative" style={{ backgroundImage: `url(${LearnImg})` }} >
+                            <h2 className='font-pop font-bold text-[#F40404] py-3.5 px-7 bg-white w-fit absolute top-[148px] left-[320px] cursor-pointergit'>Learn More</h2>
+                        </div>
                     </div>
                 </div>
             </Container>
